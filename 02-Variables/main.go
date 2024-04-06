a61ea3e760abb45c29d4a70d1d7045327f7828d5
Vijay <vijay.shankar.2110@gmail.com>
2024-04-07 02:35:03 +0530

    variables

--- 02-Variables/main.go
+++ 02-Variables/main.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
+var LoginToken = 30000 // Public variable
+
 func main() {
-	fmt.Println("Variables")
+	var username string = "Vijay"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isBool bool = true
+	fmt.Println(isBool)
+	fmt.Printf("Variable is of type: %T \n", isBool)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+
+	var smallFloat float32 = 255.65465451384364545
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
+
+	//default values of declaritive variables
+	var anotherVar int
+	fmt.Println(anotherVar)
+	fmt.Printf("Type is %T \n", anotherVar)
+
+	var anotherStr string
+	fmt.Println(anotherStr)
+	fmt.Printf("Type is %T \n", anotherStr)
+
+	fmt.Println(LoginToken)
+	fmt.Printf("type is %T", LoginToken)
+
 }
